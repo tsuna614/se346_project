a30caeecbf6a12e0026aeb846efaf4bfb3b20f78
Shoru <bapcui596@gmail.com>
2024-05-08 10:51:23 +0700

    feat(home): added bottom nav and top nav also used sample data to display posts

--- lib/src/app-screens/home_screen.dart
+++ lib/src/app-screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:se346_project/src/widgets/bottom_navigation_bar.dart';
+import 'package:se346_project/src/widgets/post.dart';
+import 'package:se346_project/src/data/sample_posts_data.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen({super.key});
@@ -9,17 +13,38 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigator(),
       appBar: AppBar(
-        title: const Text('Home Screen'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            auth.signOut();
-          },
-          child: const Text('Sign Out'),
+        //Use assets/images/logo.png then "Home Screen"
+        title: Row(
+          children: [
+            Image.asset(
+              'assets/images/Logo.png',
+              height: 30,
+            ),
+            const SizedBox(width: 8.0),
+            const Text('Homepage',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                )),
+          ],
         ),
       ),
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+          children: samplePosts
+              .map((post) => Post(
+                    name: post['name']!,
+                    content: post['content']!,
+                    onLike: () {},
+                    onComment: () {},
+                    onShare: () {},
+                  ))
+              .toList(),
+        ),
+      )),
     );
   }
 }
---
+++ lib/src/data/sample_posts_data.dart
@@ -0,0 +1,25 @@
+//Array of sample post
+import 'dart:ffi';
+
+const samplePosts = [
+  {
+    'name': 'John Doe',
+    'content':
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+  },
+  {
+    'name': 'Jane Doe',
+    'content':
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+  },
+  {
+    'name': 'John Smith',
+    'content':
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+  },
+  {
+    'name': 'Jane Smith',
+    'content':
+        'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s',
+  }
+];
