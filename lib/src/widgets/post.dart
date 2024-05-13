ce85b693fd7c1332dcb258842d3675695f33121a
Shoru <bapcui596@gmail.com>
2024-05-13 21:12:43 +0700

    improvement(post): updated post and comment widgets

--- lib/src/widgets/post.dart
+++ lib/src/widgets/post.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,6 +10,7 @@ import 'package:se346_project/src/blocs/CommentBloc.dart';
 const _avatarSize = 40.0;
 
 class Post extends StatelessWidget {
+  final String id;
   final String name;
   final String content;
   final List<dynamic> comments;
@@ -16,6 +18,7 @@ class Post extends StatelessWidget {
   final String? mediaUrl;
   const Post({
     super.key,
+    required this.id,
     required this.name,
     required this.content,
     this.comments = const [],
@@ -35,9 +38,16 @@ class Post extends StatelessWidget {
         builder: (context) => BlocProvider<CommentBloc>(
           create: (context) => CommentBloc(),
           child: CommentPage(
-            commentBloc: CommentBloc(),
-            comments: comments, // Pass the comments list here
-          ),
+              commentBloc: CommentBloc(),
+              comments: comments,
+              postData: PostData(
+                id: id,
+                name: name,
+                content: content,
+                comments: comments,
+                avatarUrl: avatarUrl,
+                mediaUrl: mediaUrl,
+              )),
         ),
       ),
     );
@@ -151,14 +161,15 @@ class Post extends StatelessWidget {
                     Text('Like'),
                   ],
                 ),
-                Row(
-                  children: [
-                    IconButton(
-                      icon: const Icon(Icons.comment),
-                      onPressed: () => onComment(context),
-                    ),
-                    Text('Comment'),
-                  ],
+                GestureDetector(
+                  onTap: () => onComment(context),
+                  child: Row(
+                    children: [
+                      Icon(Icons.comment),
+                      SizedBox(width: 4.0),
+                      Text('Comment'),
+                    ],
+                  ),
                 ),
                 Row(
                   children: [
@@ -181,9 +192,14 @@ class Post extends StatelessWidget {
 class CommentPage extends StatefulWidget {
   final CommentBloc commentBloc;
   final List<dynamic> comments; // Receive comments here
+  final PostData postData; // Pass the original post data here
 
-  const CommentPage(
-      {super.key, required this.commentBloc, required this.comments});
+  const CommentPage({
+    Key? key,
+    required this.commentBloc,
+    required this.comments,
+    required this.postData,
+  }) : super(key: key);
 
   @override
   _CommentPageState createState() => _CommentPageState();
@@ -195,8 +211,12 @@ class _CommentPageState extends State<CommentPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('Comments'),
+        title: Center(
+          //Center user name
+          child: Text(widget.postData.name),
+        ),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
@@ -204,33 +224,21 @@ class _CommentPageState extends State<CommentPage> {
           },
         ),
       ),
-      body: StreamBuilder<CommentState>(
-        stream: widget.commentBloc.commentStateStream,
-        initialData: CommentsLoaded(widget.comments),
-        builder: (context, snapshot) {
-          if (snapshot.hasData && snapshot.data is CommentsLoaded) {
-            List<dynamic> comments = (snapshot.data as CommentsLoaded).comments;
-            return ListView.builder(
-              itemCount: comments.length,
-              itemBuilder: (context, index) {
-                final comment = comments[index];
-                return CommentItem(
-                  text: comment['text'],
-                  image: comment['image'],
-                  name: comment['name'] ?? 'Anonymous',
-                  onRemove: () {
-                    widget.commentBloc.commentEventSink
-                        .add(RemoveComment(index));
-                  },
-                );
-              },
-            );
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            PostForCommentPage(
+              name: widget.postData.name,
+              content: widget.postData.content,
+              comments: widget.postData.comments,
+              commentBloc: widget.commentBloc,
+              avatarUrl: widget.postData.avatarUrl,
+              mediaUrl: widget.postData.mediaUrl,
+            ),
+            // Add some spacing
+          ],
+        ),
       ),
       bottomNavigationBar: BottomAppBar(
         child: Row(
@@ -267,72 +275,296 @@ class _CommentPageState extends State<CommentPage> {
   }
 }
 
-class CommentItem extends StatelessWidget {
+class CommentItem extends StatefulWidget {
   final String text;
   final String? image;
   final String name;
   final VoidCallback onRemove;
 
   const CommentItem({
-    super.key,
+    Key? key,
     required this.text,
     this.image,
     required this.name,
     required this.onRemove,
-  });
+  }) : super(key: key);
 
   @override
+  _CommentItemState createState() => _CommentItemState();
+}
+
+class _CommentItemState extends State<CommentItem> {
+  @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 8.0),
-      child: Card(
-        elevation: 2.0,
-        margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  CircleAvatar(
-                    radius: 20,
-                    // You can add avatarUrl logic here
-                    child: Text(name[
-                        0]), // Displaying first character of name as avatar
-                  ),
-                  const SizedBox(width: 8.0),
-                  Text(name),
-                ],
-              ),
-              const SizedBox(height: 8.0),
-              if (image != null)
-                FadeInImage.memoryNetwork(
-                  placeholder: kTransparentImage,
-                  image: image!,
-                  fit: BoxFit.cover,
-                  width: double.infinity,
-                  height: 200,
-                  imageErrorBuilder: (context, error, stackTrace) {
-                    return Container(
-                      width: double.infinity,
-                      height: 200,
-                      color: Colors.grey[300],
-                    );
-                  },
+    return Card(
+      margin: EdgeInsets.zero,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.zero,
+      ),
+      color: Colors.white,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //Comment avatar
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 20,
+                      child: Text(widget.name[0]),
+                    ),
+                  ],
                 ),
-              if (image != null) const SizedBox(height: 8.0),
-              Text(text),
-              const SizedBox(height: 8),
-              //Todo: implement user checking to allow for removal.
-              // ElevatedButton(
-              //   onPressed: onRemove,
-              //   child: Text('Remove'),
-              // ),
-            ],
-          ),
+                SizedBox(width: 8.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    //Comment name and text
+                    Container(
+                      padding: const EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).cardColor ?? Colors.grey[300],
+                        border: Border.all(
+                          //Todo apply theme later
+                          color: Colors.grey[300]!,
+                          width: 1,
+                        ),
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(widget.name,
+                              style: TextStyle(fontWeight: FontWeight.bold)),
+                          Text('2 hours ago',
+                              style: Theme.of(context).textTheme.caption),
+                          Container(
+                            width: MediaQuery.of(context).size.width * 0.7,
+                            child: Text(widget.text,
+                                overflow: TextOverflow.visible),
+                          ),
+                        ],
+                      ),
+                    ),
+                    //Comment image
+                    if (widget.image != null)
+                      Column(
+                        children: [
+                          SizedBox(height: 4.0),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: widget.image!,
+                              fit: BoxFit.cover,
+                              imageErrorBuilder: (context, error, stackTrace) {
+                                return Container(
+                                  width: 200,
+                                  height: 200,
+                                  color: Colors.grey[300],
+                                );
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                  ],
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
+}
+
+class PostForCommentPage extends StatelessWidget {
+  final String name;
+  final String content;
+  final List<dynamic> comments;
+  final String? avatarUrl;
+  final String? mediaUrl;
+  final CommentBloc commentBloc;
+
+  const PostForCommentPage({
+    super.key,
+    required this.name,
+    required this.content,
+    this.comments = const [],
+    required this.commentBloc,
+    this.avatarUrl,
+    this.mediaUrl,
+  });
+
+  void onLike() {
+    // Handle like action
+  }
+  void onShare() {
+    // Handle share action
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(0.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                if (avatarUrl != null)
+                  CircleAvatar(
+                    radius: _avatarSize / 2,
+                    backgroundImage: NetworkImage(avatarUrl!),
+                  )
+                else
+                  CircleAvatar(
+                    radius: _avatarSize / 2,
+                    backgroundColor: Colors.primaries[
+                        DateTime.now().microsecondsSinceEpoch %
+                            Colors.primaries.length],
+                  ),
+                const SizedBox(width: 8.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(name),
+                    Text(
+                      '2 hours ago',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            const SizedBox(height: 8.0),
+            Column(
+              children: [
+                Text(content),
+                if (mediaUrl != null)
+                  FadeInImage.memoryNetwork(
+                    placeholder: kTransparentImage,
+                    image: mediaUrl!,
+                    fit: BoxFit.cover,
+                    width: double.infinity,
+                    height: 200,
+                    imageErrorBuilder: (context, error, stackTrace) {
+                      return Container(
+                        width: double.infinity,
+                        height: 200,
+                        color: Colors.grey[300],
+                      );
+                    },
+                  ),
+              ],
+            ),
+            const SizedBox(height: 8.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Icon(
+                      Icons.thumb_up,
+                      size: 16.0,
+                    ),
+                    const SizedBox(width: 4.0),
+                    Text(
+                      '1',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Text(
+                      '${comments.length} comments',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                    const SizedBox(width: 8.0),
+                    Text(
+                      '0 shares',
+                      style: Theme.of(context).textTheme.caption,
+                    ),
+                  ],
+                ),
+                //Remove comment button
+              ],
+            ),
+            Divider(
+              color: Color.fromARGB(123, 158, 158, 158),
+              thickness: 1,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.thumb_up),
+                      onPressed: onLike,
+                    ),
+                    Text('Like'),
+                  ],
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.share),
+                      onPressed: onShare,
+                    ),
+                    Text('Share'),
+                  ],
+                ),
+              ],
+            ),
+            //Add comments
+            Divider(
+              color: Color.fromARGB(123, 158, 158, 158),
+              thickness: 1,
+            ),
+            for (var comment in this.comments)
+              CommentItem(
+                text: comment['text'],
+                image: comment['image'],
+                name: comment['name'],
+                onRemove: () {
+                  this
+                      .commentBloc
+                      .commentEventSink
+                      .add(RemoveComment(comment['id']));
+                },
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class PostData {
+  final String id;
+  final String name; //poster
+  final String content;
+  final List<dynamic> comments;
+  final String? avatarUrl;
+  final String? mediaUrl;
+
+  PostData({
+    required this.id,
+    required this.name,
+    required this.content,
+    this.comments = const [],
+    this.avatarUrl,
+    this.mediaUrl,
+  });
 }
