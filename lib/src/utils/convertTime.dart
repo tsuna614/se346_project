9d8be7b6b19f67b24ecb7690483ee16a7ff4510d
Shoru <bapcui596@gmail.com>
2024-06-10 19:42:44 +0700

    fix(post): time display cleanup

--- lib/src/utils/convertTime.dart
+++ lib/src/utils/convertTime.dart
@@ -10,6 +10,7 @@ String convertTime(DateTime time) {
   } else if (difference.inDays < 7) {
     return '${difference.inDays} days ago';
   } else {
-    return time.toString();
+    //Only YYYY-MM-DD . Add zero if month or day is less than 10
+    return '${time.year}-${time.month < 10 ? '0${time.month}' : time.month}-${time.day < 10 ? '0${time.day}' : time.day}';
   }
 }
